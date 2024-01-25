c0ca7ce34d0467e7d9c2307c54644764552107c3
Nureka Rodrigo <cst20069@std.uwu.ac.lk>
2024-01-25 23:10:58 +0530

    Done UI

--- app/src/main/java/com/example/kotlin_weather_app/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_weather_app/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.kotlin_weather_app
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.kotlin_weather_app.adapters.HourlyForecastAdapter
+import com.example.kotlin_weather_app.adapters.WeatherItem
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
+
+        val itemList = listOf(
+            WeatherItem("Now", R.drawable.cloudy_sunny, "25°"),
+            WeatherItem("9 AM", R.drawable.cloudy_sunny, "25°"),
+            WeatherItem("12 PM", R.drawable.cloudy_sunny, "25°"),
+            WeatherItem("3 PM", R.drawable.cloudy_sunny, "25°"),
+            WeatherItem("6 PM", R.drawable.cloudy_sunny, "25°"),
+            WeatherItem("9 PM", R.drawable.cloudy_sunny, "25°"),
+            WeatherItem("12 AM", R.drawable.cloudy_sunny, "25°"),
+            WeatherItem("3 AM", R.drawable.cloudy_sunny, "25°"),
+            WeatherItem("6 AM", R.drawable.cloudy_sunny, "25°"),
+            )
+
+        val adapter = HourlyForecastAdapter(itemList)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
     }
 }
---
+++ app/src/main/java/com/example/kotlin_weather_app/adapters/HourlyForecastAdapter.kt
@@ -0,0 +1,37 @@
+package com.example.kotlin_weather_app.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.kotlin_weather_app.R
+
+data class WeatherItem(val title: String, val iconResId: Int, val temperature: String)
+
+class HourlyForecastAdapter(private val itemList: List<WeatherItem>) : RecyclerView.Adapter<HourlyForecastAdapter.ViewHolder>() {
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val titleTextView: TextView = itemView.findViewById(R.id.textView12)
+        val iconImageView: ImageView = itemView.findViewById(R.id.imageView5)
+        val temperatureTextView: TextView = itemView.findViewById(R.id.textView13)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.viewholder_hourly, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val currentItem = itemList[position]
+        holder.titleTextView.text = currentItem.title
+        holder.iconImageView.setImageResource(currentItem.iconResId)
+        holder.temperatureTextView.text = currentItem.temperature
+    }
+
+    override fun getItemCount(): Int {
+        return itemList.size
+    }
+}
