3bf39c378159b54b834b6d936b76632f18afc602
kavindu-mane <99143916+kavindu-mane@users.noreply.github.com>
2024-03-22 16:01:03 +0530

    add datastore dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,4 +51,5 @@ dependencies {
     implementation("androidx.core:core-splashscreen:1.1.0-alpha02")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
     implementation("androidx.activity:activity-ktx:1.8.2")
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
 }
